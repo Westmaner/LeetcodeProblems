54753c85674cec08782d0913c8190c6bf2a8aa88
stimyoon <64033604+stimyoon@users.noreply.github.com>
2023-06-18 15:43:29 -0400

    Initial Commit

---
+++ LeetcodeProblems/LeetcodeProblemsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeetcodeProblemsApp.swift
+//  LeetcodeProblems
+//
+//  Created by Tim Yoon on 6/18/23.
+//
+
+import SwiftUI
+
+@main
+struct LeetcodeProblemsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
